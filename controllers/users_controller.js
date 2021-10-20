381c6589915944330c58678ac0fd47affd584761
Yianni Catege <ycatege@gmail.com>
2021-10-20 16:51:25 -0400

    users can be created and stored in the DB

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,13 +6,13 @@ const bcrypt = require('bcrypt')
 //INDEX ROUTE
 users.get('/new',(req,res)=>{
     res.render('users/new.ejs',{
-        currentUser:req.session.currentUser
+        // currentUser:req.session.currentUser
     })
 })
 
 //POST ROUTE
 users.post('/',(req,res)=>{
-    req.body.password=bcrypt.hashsync(req.body.password,bcrypt.genSaltSync(10))
+    req.body.password=bcrypt.hashSync(req.body.password,bcrypt.genSaltSync(10))
     User.create(req.body,(err,createdUser)=>{
         console.log(createdUser)
         res.redirect('/')
